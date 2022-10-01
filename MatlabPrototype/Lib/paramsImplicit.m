8c43f9f6493885e902c4d22442b9574f9ac0cedd
Matteo Insarauto <113996048+insarcar@users.noreply.github.com>
2022-10-01 10:13:18 +0200

    Update paramsImplicit.m

--- MatlabPrototype/Lib/paramsImplicit.m
+++ MatlabPrototype/Lib/paramsImplicit.m
@@ -1,7 +1,7 @@
 function [threshold, slope] = paramsImplicit(compLevel)
 %trasformazione dei parametri espliciti nei parametri impliciti interni
     
-    %level [0, 12] ---> threshold [0dB, -28dB circa] negative gompertz
+    %level [0, 12] ---> threshold [0dB, -33dB circa] negative gompertz
     a = -0.999999; b = -15; c = -0.55;
     threshold = 1 + a * exp(b * exp(c * compLevel));
     threshold = mag2db(threshold);
